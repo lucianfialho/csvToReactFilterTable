001b81d733ac7e35e58068c9f3328190366cf31a
Lucian Fialho <lucianfialhobp@hotmail.com>
2021-09-21 15:33:16 -0300

    -- mudando title

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Faça orçamento de obras com no SINAP mais rápido - Orça Obra Fácil</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
